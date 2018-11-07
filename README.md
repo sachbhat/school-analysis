d32a5a6e1201c5604e0cabe4a007888af0599c46
Sachin Bhatia <sachinb@gmail.com>
2018-11-07 10:45:04 -0800

    Renamed scratchpad, added documentation to helpers, READEM

--- README.md
+++ README.md
@@ -1,15 +1,58 @@
-# school-analysis
+# Washington State School Analysis
+
+This repository contains Jupyter notebooks and helper scripts in Python to download, process, and analyze
+data from the Office of the Superintendent of Public Instruction (OSPI) for the State of Washington, as hosted on [http://www.k12.wa.us/](http://www.k12.wa.us/).
 
 ## Overview
 
-I'll add more details soon, but this is a work-in-progress on a public dataset that I'm using to learn Python, Pandas, NumPy, and the various graphing libraries. It also helps that I'm interested in the dataset and exploring the problem space around education.
+My analysis handles OSPI data for three years (2016, 2017, and 2018) in the following categories:
 
-There are a few files so far:
+- [Smarter Balanced Assessment (SBA)](http://www.k12.wa.us/smarter/) - an assessment consortium that is used in Washington, with support of the state legislature. These are required tests that happen at beginning and end of school year in grades 3 and up.
 
-- [helpers.py](./helpers.py) : As the name describes, a set of python helper functions that I'm using in the notebook.
+- [Washington Kindergarten Inventory of Developing Skills (WaKids)](http://www.k12.wa.us/WaKIDS/Assessment/default.aspx) - not a test, but an assessment by kindergarten teachers based on observation during everyday classroom activities. Before Oct 31, each child is assessed on 6 factors: social-emotional, physical, cognitive, language, literacy, and mathematics.
 
-- [Exploratory Analysis - School Data.ipynb](Exploratory%20Analysis%20-%20School%20Data.ipynb): This is a playground to understand underlying datsets
+While I have yet to analyze the following category, some of the basic data wrangling also exists for the following data source:
 
-- [Test Success Rates of 3rd Graders in Washington.ipynb](Test%20Success%20Rates%20of%203rd%20Graders%20in%20Washington.ipynb): This is an attempt at telling a story with the data. It's too early to tell what that story is.
+- [WA-AIM](http://www.k12.wa.us/assessment/WA-AIM/default.aspx): this is an acronym for Washington Access to Instruction and Measurement - an alternative achievement standard that is aligned to Common Core Standards for students with significant cognitive challenges.
 
-Enjoy the view. And let me know if you have questions.
+All of the data sources point at original web files. I have not done any local caching of the files. Thus, to explore further, you need access to a live internet connection.
+
+## Dependencies
+
+- All code is written in Python 3
+- All code depends on the following libraries:
+  - `numpy`
+  - `pandas`
+  - `matplotlib`
+  - `seaborn`
+
+## Description of files
+
+Non-python files:
+
+|----------|-------------|
+| filename | description |
+|----------|-------------|
+| README.md| Text file (markdown format) description of project |
+| data/*.txt | Local cache of files referenced (unused) |
+|----------|-------------|
+
+Python modules:
+
+|----------|-------------|
+| filename | description |
+|----------|-------------|
+| helpers.py | Helper functions to load and transform datasets; this helps provide clarity to notebooks |
+|----------|-------------|
+
+Jupyter notebooks:
+
+|----------|-------------|
+| filename | description |
+|----------|-------------|
+| [Test Success Rates of 3rd Grades in Washington.py](./Test Success Rates of 3rd Grades in Washington.py) | How well do 3rd graders do across different demographics? |
+| Pre-K Prep Stats.ipynb | How well prepared are kids by the time they reach Kindergarten? |
+| Correlations.ipynb | What are the factors that most correlate with test succcess? |
+| YoY Consistency - Higher Performing Test Scores.ipynb | How well do high perorming schools continue this performance? |
+| Scratchpad.ipynb | This is my scratchpad - :-) |
+|----------|-------------|
--- helpers.py
+++ helpers.py
@@ -4,6 +4,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def load_dataset(year=2018):
+    """
+    Load OSPI data set of assemesment performance of Washington state public schools.
+
+    Currently supports 2016, 2017, and 2018.
+
+    Parameters
+    ----------
+    year : int
+        Year
+
+    Returns
+    -------
+    pandas.dataframe
+        Preloaded dataframe based on CSV downloaded from reportcard.ospi.k12.wa.us
+    """
+
     url_map = {2018: 'http://reportcard.ospi.k12.wa.us/Reports/2018/2_03_AIM-WCAS-SBA%20Assessments%20School%20(with%20suppression%20-%20new%20format).txt',
         2017: 'http://reportcard.ospi.k12.wa.us/Reports/2017/2_03_AIM-EOC-MSP-SBA%20Assessments%20School%20(with%20suppression%20-%20new%20format).txt',
         2016: 'http://reportcard.ospi.k12.wa.us/Reports/2016/2_03_AIM-EOC-MSP-SBA%20Assessments%20School%20(with%20suppression%20-%20new%20format).txt'}
@@ -15,6 +31,24 @@ def load_dataset(year=2018):
 
 # Take in a integer and slice data feed by that grade. If we have no information, return None.
 def filter_by_grade(df, grade=None):
+    """
+    Filter OSPI data by grade
+
+    Supports 3-11
+
+    Parameters
+    ----------
+    df : pandas.dataframe
+        Input dataframe
+
+    grade : int
+        grade level (3-11)
+
+    Returns
+    -------
+    pandas.dataframe
+        Preloaded dataframe filtered by grade
+    """
 
     grade_map = {5: '5th', 4: '4th', 3: '3rd', 8: '8th', 6: '6th', 7: '7th', 10: '10th', 11: '11th'}
 
@@ -25,6 +59,25 @@ def filter_by_grade(df, grade=None):
 
 # Take in 'math' or 'english' and filter out data that matches
 def filter_by_subject(df, subject=None):
+    """
+    Filter OSPI data by subject
+
+    Supports 2 different math tests, english
+
+    Parameters
+    ----------
+    df : pandas.dataframe
+        Input dataframe
+
+    subject : string
+        'math', 'math1', 'english'
+
+    Returns
+    -------
+    pandas.dataframe
+        Preloaded dataframe filtered by subject
+    """
+
     subject_map = {'math': 'MATH', 'math1':'Math', 'english': 'ELA'}
 
     if subject in subject_map:
@@ -32,16 +85,70 @@ def filter_by_subject(df, subject=None):
     else:
         return df[df['Subject']==0]
 
-def filter_by_grade_subject_student_group(df, grade, subject, group, value):
+def get_value_by_grade_subject_student_group(df, grade, subject, group, value):
+    """
+    Get single value of OSPI data by subject, grade, student group
+
+    Supports 2 different math tests, english, grades 3-11, and any student group represented in underlying load_dataset
+
+    Parameters
+    ----------
+    df : pandas.dataframe
+        Input dataframe
+
+    grade : int
+        grade level (3-11)
+
+    subject : string
+        'math', 'math1', 'english'
+
+    student_group : string
+        demographic group (e.g. "All", "Low Income")
+
+    value : string
+        numeric value data should return
+
+    Returns
+    -------
+    pandas.dataframe
+        Preloaded dataframe 3 clolumns district, school, and value
+    """
+
     df_ = filter_by_grade(df, grade)
     df_ = filter_by_subject(df_, subject)
     df_ = df_.loc[(df_["StudentGroup"]==group)]
     df_ = df_.loc[:, ['District', 'School', value]]
-    return df_    
+    return df_
 
 # create pivot table of school x studentGroup with given values
 def pivot_by_grade_subject_student_group(df, grade, subject, groups, values):
+    """
+    Create pivot table of school x student group with given values
 
+    Supports 2 different math tests, english, grades 3-11, and any student group represented in underlying load_dataset
+
+    Parameters
+    ----------
+    df : pandas.dataframe
+        Input dataframe
+
+    grade : int
+        grade level (3-11)
+
+    subject : string
+        'math', 'math1', 'english'
+
+    groups : array of strings
+        demographic group (e.g. ["All", "Low Income"])
+
+    values : array of string
+        numeric value datas should return
+
+    Returns
+    -------
+    pandas.dataframe
+        Preloaded dataframe 3 clolumns district, school, and value
+    """
     # filter by grade
     df = filter_by_grade(df, grade)
 
@@ -58,7 +165,6 @@ def pivot_by_grade_subject_student_group(df, grade, subject, groups, values):
     df["DistrictSchool"] = df.apply(lambda row: "{}:{}".format(row.District, row.School), axis=1)
 
     #display(df.head())
-
     # grab the values we care about in pivoted form
     df_pivot = df.pivot(index = "DistrictSchool", columns="StudentGroup", values=values)
 
@@ -66,7 +172,27 @@ def pivot_by_grade_subject_student_group(df, grade, subject, groups, values):
 
     return df_pivot[:]
 
+
 def build_visualization(df, x0, y0):
+    """
+    Quick joinplot of two variable columns.
+
+    Parameters
+    ----------
+    df : pandas.dataframe
+        Input dataframe
+
+    x0 : string
+        label for x-axis
+
+    y0 : string
+        label for y-axis
+
+    Returns
+    -------
+    nil
+    """
+
     plt.figure(figsize=(10,10))
     sns.set_style('darkgrid')
 
@@ -78,6 +204,33 @@ def build_visualization(df, x0, y0):
 
 
 def compare_groups(data, grade, subject, groups, values):
+    """
+    Quick visualization of scores across two different groups
+
+    Supports 2 different math tests, english, grades 3-11, and any student group represented in underlying load_dataset
+
+    Parameters
+    ----------
+    df : pandas.dataframe
+        Input dataframe
+
+    grade : int
+        grade level (3-11)
+
+    subject : string
+        'math', 'math1', 'english'
+
+    groups : string
+        demographic group (e.g. "All", "Low Income")
+
+    values : array of string (should be 2)
+        numeric value datas should return
+
+    Returns
+    -------
+    pandas.dataframe
+        Preloaded dataframe 2 columns x, y
+    """
 
     df = pivot_by_grade_subject_student_group(data, grade, subject, groups, values)
 
